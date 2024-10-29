7e7c6b018de62090df39cbf921272cf85446569d
isaac omar <isaacomar29@gmail.com>
2024-10-29 18:59:41 +0100

    V4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 
 
 
 export default defineConfig({
-  site: 'https://isaac2910.github.io',
-  base: 'portfolio',
+  output: 'server',
+  adapter: vercel(),
+
 })
