7c07d72a56a62585bcf953865e26a4329a78cbef
isaac omar <isaacomar29@gmail.com>
2025-02-13 14:18:05 +0100

    v12

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import react from "@astrojs/react";
 // https://astro.build/config
 
 
@@ -8,5 +9,8 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [react()],
 
 })
+
+
